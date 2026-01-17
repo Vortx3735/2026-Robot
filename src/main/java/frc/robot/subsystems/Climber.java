06ecb1ab7872da81cce0c3ec1f02265c35cb30ca
teralithtechnologies <teralithtechnologies@gmail.com>
2026-01-17 10:38:22 -0600

    what

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -8,60 +8,48 @@ import com.ctre.phoenix6.hardware.TalonFX;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-// import stuff up here
-// for example:
-// import com.aaronsFavWebsite.motorLibrary.Motor;
 
 public class Climber extends SubsystemBase {
-  /*define objects and variables here (e.g. motors, sensors, variables)
-   *for example:
-   *public final Motor motor1;
-   */
+
   private final TalonFX climberMotor1;
   private double speed = 0.25;
-  private final TalonFX climberMotor2;
-  /*initialize subsystem objects in constructor
-   *for good practice, pass in any constants through the constructor
-   */
+
   public Climber(int motorId) {
     climberMotor1 = new TalonFX(motorId);
   }
-  public double getSpeed(){
+
+  public double getSpeed() {
     return speed;
   }
 
-
   public void setSpeed(double speed) {
-    /*example method for the subsystem
-     *for example, set motor speed, read sensor value, etc.
-     *example:
-     *this.motor1.setSpeed(0.5);
-     */
-    this.speed=speed;
+    this.speed = speed;
   }
 
-  /*an example method that returns a very basic command
-   *only create commands in the subystem if they only utilize methods from the same subystem
-   */
-  public Command exampleMethodCommand() {
-    /*return an inline command
-     *this example uses runOnce, but you can also use run or startEnd depending on your needs
-     */
-    return this.runOnce(
+  public void up() {
+    climberMotor1.set(speed);
+  }
+  public void down() {
+    climberMotor1.set(-speed);
+  }
+  public void stop() {
+    climberMotor1.set(0);
+  }
+  public Command upCommand() {
+
+    return this.run(
         () ->
-            // single method goes here
-            // for example:
-            this.exampleMethod());
+  
+            this.up());
+  }
+public Command downCommand() {
+
+    return this.run(
+        () ->
+      
+            this.down());
   }
 
-  // an example getter for the motors, sensors,variables, etc. of the subsystem
-  public boolean getSomeValue() {
-    /*return some value from the subsystem
-     *for example:
-     *return this.motor.getSpeed()
-     */
-    return false;
-  }
 
   @Override
   public void periodic() {
