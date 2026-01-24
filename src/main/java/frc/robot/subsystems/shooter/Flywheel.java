8ca0f36f331036aa7a321f864898fe83b52e1f1a
teralithtechnologies <teralithtechnologies@gmail.com>
2026-01-23 19:18:31 -0600

    Added Network Table

--- src/main/java/frc/robot/subsystems/shooter/Flywheel.java
+++ src/main/java/frc/robot/subsystems/shooter/Flywheel.java
@@ -1,10 +1,17 @@
 package frc.robot.subsystems.shooter;
 
 import com.ctre.phoenix6.hardware.TalonFX;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Flywheel extends SubsystemBase {
+  private final NetworkTable table =
+      NetworkTableInstance.getDefault().getTable("Elastic").getSubTable("Flywheel");
+  private final NetworkTableEntry ntActualRPM = table.getEntry("ActualRPM");
+  private final NetworkTableEntry ntTargetRPM = table.getEntry("TargetRPM");
   private static TalonFX flywheelMotor;
   private double motorSpeed;
 
@@ -34,5 +41,25 @@ public class Flywheel extends SubsystemBase {
 
   public Command shootCommand() {
     return run(() -> shoot());
+  }
+
+  @Override
+  public void periodic() {
+    publishTelemetry();
+    readDashboardControls();
+  }
+
+  private void readDashboardControls() {
+    // Read the target RPM from the Elastic slider
+    motorSpeed = ntTargetRPM.getDouble(0);
+  }
+
+  private void publishTelemetry() {
+
+    // Read the current flywheel speed from the encoder
+    double currentRPM = motorSpeed;
+
+    // Publish the measured speed to Elastic
+    ntActualRPM.setDouble(currentRPM);
   }
 }
