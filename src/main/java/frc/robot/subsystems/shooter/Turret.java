8619611f1357f02360a3a6848cf7c2f672e10a5a
teralithtechnologies <teralithtechnologies@gmail.com>
2026-02-16 17:57:21 -0600

    spacezzzz

--- src/main/java/frc/robot/subsystems/shooter/Turret.java
+++ src/main/java/frc/robot/subsystems/shooter/Turret.java
@@ -15,7 +15,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.Mode;
 import org.littletonrobotics.junction.Logger;
-
+    
 public class Turret extends SubsystemBase {
   public static TalonFX turretMotor;
   public double turretPosition;
